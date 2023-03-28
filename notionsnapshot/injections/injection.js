9488b93448cbbf0e2fc1799f9c75752cd871a2bc
Yahya Jabary <jabaryyahya@gmail.com>
2023-03-28 04:08:41 +0200

    add docs, update comments in injections

--- notionsnapshot/injections/injection.js
+++ notionsnapshot/injections/injection.js
@@ -1,5 +1,5 @@
 /**
- * re-implements toggle buttons (we remove the original event listeners)
+ * re-implement toggle buttons
  */
 const showToggle = (content, arrow) => {
   arrow.style.transform = "rotateZ(180deg)";
@@ -29,7 +29,7 @@ for (toggleButton of toggleButtons) {
 }
 
 /**
- * re-implements anchor links (we remove the original event listeners)
+ * re-implement anchor links
  */
 const anchorLinks = document.querySelectorAll("a.notionsnapshot-anchor-link");
 for (anchorLink of anchorLinks) {
@@ -46,44 +46,13 @@ for (anchorLink of anchorLinks) {
   });
 }
 
-
 /**
- * sets all iframes' parent container opacity to 1
- * originally notion has a callback to do that on iframe loaded
+ * set all iframes' parent container opacity to 1
  */
 const pendingIframes = document.getElementsByTagName("iframe");
 for (let i = 0; i < pendingIframes.length; i++) {
   pendingIframes.item(i).parentElement.style.opacity = 1;
 }
-
-// const pendingIframes = document.getElementsByClassName("notionsnapshot-iframe-target");
-// for (let i = 0; i < pendingIframes.length; i++) {
-//   const pendingIframe = pendingIframes.item(i);
-//   const iframeSrc = pendingIframe.getAttribute("notionsnapshot-iframe-src");
-//   const iframe = document.createElement("iframe");
-
-//   pendingIframe.style.opacity = 0;
-//   iframe.onload = () => {
-//     pendingIframe.style.opacity = 1;
-//   };
-
-//   iframe.style.width = "100%";
-//   iframe.style.height = "100%";
-//   iframe.style.position = "absolute";
-//   iframe.style.left = 0;
-//   iframe.style.top = 0;
-//   iframe.style.pointerEvents = "auto";
-
-//   iframe.setAttribute("src", iframeSrc);
-//   iframe.setAttribute("frameborder", "0");
-//   iframe.setAttribute(
-//     "sandbox",
-//     "allow-scripts allow-popups allow-top-navigation-by-user-activation allow-forms allow-same-origin"
-//   );
-
-//   pendingIframe.appendChild(iframe);
-// }
-
 
 /**
  * hide search box on inline databases
@@ -95,7 +64,7 @@ for (let i = 0; i < collectionSearchBoxes.length; i++) {
 }
 
 /**
- * removes extra padding in Webkit renderers on iOS devices
+ * remove extra padding in Webkit renderers on iOS devices
  */
 const imgs = document.querySelectorAll("img:not(.notion-emoji)");
 for (let i = 0; i < imgs.length; i++) {
